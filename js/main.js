6b370d97625d0a03e17d10cdc6710bd02381e72d
Fabio <fabio.mezzina.09@gmail.com>
2020-10-27 15:55:11 +0100

    aggiunto scroll con tastiera

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
+// references
 var images = $('.images');
 var imgPrecedente = $('.prev');
 var imgSuccessiva = $('.next');
-
+// evento click per immagine precedente
 imgPrecedente.click( function() {
   scroll('prec');
 });
-
+// evento click per immagine successiva
 imgSuccessiva.click( function() {
   scroll('succ');
+});
+// evento scroll immagini con freccette tastiera
+$(document).keydown(function (event) {
+  switch (event.keyCode) {
+    case 37:
+      scroll('prec');
+      break;
+    case 39:
+      scroll('succ');
+      break;
+  }
 });
 
 // DICHIARAZIONE FUNZIONE
 function scroll (direction) {
   var imgAttuale = $('.images img.visible');
-
   if (direction === 'prec') {
     if (imgAttuale.hasClass('first')) {
       imgAttuale.removeClass('visible');
